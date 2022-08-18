ff61c39a62573b7c2086a7a647c1ecd41f9eb342
Hariz Mohamad Reza <harysreza21@gmail.com>
2022-08-18 09:37:22 +0700

    styling homepage

--- src/components/Homepage/main.homepage.js
+++ src/components/Homepage/main.homepage.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Background from "../../assets/cloud.jpg";
 
 const Homepage = () => {
   return (
     <React.Fragment>
-      <Container className="mt-5 px-5">
-        <h1 className="display-4 text-black">
-          “I am always doing that which I cannot do, in order that I may learn
-          how to do it.”
-        </h1>
-        <p
-          className="lead text-end mt-5 text-black"
-          style={{ fontWeight: "500" }}
+      <Container>
+        <Container
+          className="mt-4 p-5 rounded"
+          style={{
+            backgroundImage: `linear-gradient(to right bottom, rgba(23, 74, 144, 0.8), rgba(142, 226, 178, 0.6)), url(${Background})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            color: "rgba(0, 0, 0, 0.8)",
+          }}
         >
-          - Pablo Picasso
-        </p>
+          <h1 className="display-4 fst-italic">
+            “I am always doing that which I cannot do, in order that I may learn
+            how to do it.”
+          </h1>
+          <p className="lead text-end mt-5 fw-semibold">- Pablo Picasso</p>
+        </Container>
       </Container>
     </React.Fragment>
   );
