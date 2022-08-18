666b86d7b89192a2572447325d9fdcdae6daa70f
Hariz Mohamad Reza <harysreza21@gmail.com>
2022-08-18 10:03:38 +0700

    fix 404 page

--- src/components/404/main.404.js
+++ src/components/404/main.404.js
@@ -4,21 +4,23 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <Container>
-      <div className="d-flex align-items-center justify-content-center vh-100">
-        <div className="text-center">
-          <h1 className="display-1 fw-bold">404</h1>
-          <p className="fs-3">
-            {" "}
-            <span className="text-danger">Opps!</span> Page not found.
-          </p>
-          <p className="lead">The page you’re looking for doesn’t exist.</p>
-          <Link to="/" className="btn btn-dark">
-            Go Home
-          </Link>
+    <React.Fragment>
+      <Container>
+        <div className="d-flex align-items-center justify-content-center vh-100">
+          <div className="text-center">
+            <h1 className="display-1 fw-bold">404</h1>
+            <p className="fs-3">
+              {" "}
+              <span className="text-danger">Opps!</span> Page not found.
+            </p>
+            <p className="lead">The page you’re looking for doesn’t exist.</p>
+            <Link to="/" className="btn btn-dark">
+              Go Home
+            </Link>
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </React.Fragment>
   );
 };
 
